aa55c98a33750abba374d29a004fa43806605492
only-we <oneonlywe@gmail.com>
2019-08-30 12:14:54 +0500

    edit

---
+++ menu/script.js
@@ -0,0 +1,9 @@
+function menu () {
+	var navbar = document.getElementById("barnav");
+	navbar.style.display = "block";
+};
+
+function closed () {
+	var navbar = document.getElementById("barnav");
+	navbar.style.display = "none";
+};
